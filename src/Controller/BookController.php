c05778ffbe70f70215f208cf686580f8bf9b5aae
nikhil <nikhildongre6@gmail.com>
2020-08-31 23:02:45 +0530

    Edit functionality added for book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,7 +5,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -39,8 +38,7 @@ class BookController extends AbstractController{
      * @Route("/Book/add")
      */
     public function addBook(Request $request){
-       $book = new Book();
-       
+       $book = new Book();     
        $form = $this->createFormBuilder($book)
     ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
     ->add('author', TextType::class, array('attr' => array('class' => 'form-control')))
@@ -55,15 +53,49 @@ class BookController extends AbstractController{
 
     if($form->isSubmitted() && $form->isValid()) {
         $book = $form->getData();
-
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($book);
         $entityManager->flush();
-
         return $this->redirectToRoute('books_data');
     }
-
     return $this->render('Book/addBook.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/Book/edit/{id}", name="edit_book")
+     */
+    public function edit(Request $request, $id){
+      $book = new Book();
+      $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+    
+      $form = $this->createFormBuilder($book)
+      ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+      ->add('author', TextType::class, array(
+        'attr' => array('class' => 'form-control')
+      ))
+      ->add('department', TextType::class, array(
+        'attr' => array('class' => 'form-control')
+      ))
+      ->add('save', SubmitType::class, array(
+        'label' => 'Update',
+        'attr' => array('class' => 'btn btn-primary mt-3')
+      ))
+      ->getForm();
+
+      $form->handleRequest($request);
+     
+      if($form->isSubmitted() && $form->isValid()) {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        return $this->redirectToRoute('books_data');
+      }
+
+      return $this->render('Book/editBook.html.twig', array(
+        'form' => $form->createView()
+      ));
+
     }
 
 }
