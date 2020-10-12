893463ec073ff9494c849503e444ff89b9dc1257
Nikhil <nikhildongre6@gmail.com>
2020-10-12 20:44:38 +0530

    added eslint, sass and gulp functionality

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglifycss = require('gulp-uglifycss');
+
+gulp.task('sass', async () => gulp.src('public/sass/*.scss')
+  .pipe(sass()).pipe(gulp.dest('public/sass')));
+
+gulp.task('css', async () => {
+  gulp.src('public/sass/*.css')
+    .pipe(uglifycss({
+      uglyComments: true,
+    }))
+    .pipe(gulp.dest('dist/'));
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,45 @@
-$(document).ready(function(){
+function myFunction() {
+  const x = document.getElementById('topNav');
+  if (x.className === 'navbar') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navbar';
+  }
+}
 
-  // index page slider 
+// eslint-disable-next-line no-undef
+$(document).ready(() => {
+// index page slider
+  $('.intro').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    cssEase: 'linear',
+    autoplay: true,
+    arrows: false,
+  });
 
-        $('.intro').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: false,
-          cssEase: 'linear',
-          autoplay: true,
-          arrows: false,
-        });
-        
   // view_book page show book details on info button click
+  $('.show_book_details').click(function (e) {
+    const bookTitle = $(this).parent().parent().find('.card-header').html();
+    const bookAuthor = $(this).parent().find('.card-title').html();
+    const departmentName = $(this).parent().find('.card-text').html();
 
-      $('.show_book_details').click(function(e) {
-        
-        let book_title = $(this).parent().parent().find('.card-header').html();
-        let book_author = $(this).parent().find('.card-title').html();
-        let department_name = $(this).parent().find('.card-text').html();
-          
-        let emptyString = "<p> </p>";
-        $(".bookTitle").html(emptyString + book_title); 
-        $(".bookAuthor").html(emptyString + book_author);   
-        $(".bookDepartment").html(emptyString + department_name);   
+    const emptyString = '<p> </p>';
+    $('.bookTitle').html(emptyString + bookTitle);
+    $('.bookAuthor').html(emptyString + bookAuthor);
+    $('.bookDepartment').html(emptyString + departmentName);
 
-        $(".book_details_popup").addClass("is-on");           
-      });  
+    $('.book_details_popup').addClass('is-on');
+  });
 
-      $('.close').click(function(e) {
-        $(".book_details_popup").removeClass("is-on");
-      });  
-  
+  $('.close').click(() => {
+    $('.book_details_popup').removeClass('is-on');
+  });
+
   // show welcome message to user on /index page o
 
-      if ( $(location).attr('pathname') == "/index" ) {
-
-      //  alert("Thank you xyz \n for coming to our site");
-        
-      }
-
+  if ($(location).attr('pathname') === '/index') {
+    //  alert("Thank you xyz \n for coming to our site");
+  }
 });
-
- 
-
-  
